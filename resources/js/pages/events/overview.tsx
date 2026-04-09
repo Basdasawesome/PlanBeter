c3ea842680ac47961756cd30ba05f40dce6dd121
Ezra Vos <eevos@st.noorderpoort.nl>
2026-04-09 12:45:20 +0200

    Update - Removed unnessecary data and footer.

--- resources/js/pages/events/overview.tsx
+++ resources/js/pages/events/overview.tsx
@@ -1,10 +1,8 @@
-import { TrendingUp } from "lucide-react"
 import { Bar, BarChart, XAxis, YAxis } from "recharts"
 import {
     Card,
     CardContent,
     CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
@@ -17,20 +15,9 @@ import {
 import AppLayout from '@/layouts/app-layout';
 import type { Event } from '@/types';
 
-export const description = "A horizontal bar chart"
-
-const chartData = [
-    { month: "January", desktop: 186 },
-    { month: "February", desktop: 305 },
-    { month: "March", desktop: 237 },
-    { month: "April", desktop: 73 },
-    { month: "May", desktop: 209 },
-    { month: "June", desktop: 214 },
-]
-
 const chartConfig = {
     desktop: {
-        label: "Desktop",
+        label: "Availability",
         color: "var(--chart-1)",
     },
 } satisfies ChartConfig
@@ -69,11 +56,6 @@ export function ChartBarHorizontal(title: string, description: string | null, ch
                     </BarChart>
                 </ChartContainer>
             </CardContent>
-            <CardFooter className="flex-col items-start gap-2 text-sm">
-                <div className="flex gap-2 leading-none font-medium">
-                    Beter plannen met planBeter <TrendingUp className="h-4 w-4" />
-                </div>
-            </CardFooter>
         </Card >
     )
 }
